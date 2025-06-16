f221fcf5302bcca921e815bc9d8c91d6d21bf2e2
claurennt <c.benedetti1989@gmail.com>
2025-06-16 18:35:19 +0200

    feat: add basic input validation

--- src/components/userMove/Input.tsx
+++ src/components/userMove/Input.tsx
@@ -1,22 +1,31 @@
 import type { ComputerCoordinates } from '../../globalTypes';
+import { createGridValues, getAllPossibleCoordinates } from '../utils';
 import './Input.css';
+import { validateUserInput } from './utils';
 
 type UserInputProps = {
-  computerCordinates: ComputerCoordinates;
   setComputerCoordinates: React.Dispatch<
     React.SetStateAction<ComputerCoordinates>
   >;
   userInput: string;
   setUserInput: React.Dispatch<React.SetStateAction<string>>;
 };
+
+const rows = createGridValues({ type: 'row' });
+const columns = createGridValues({ type: 'column' });
+const allCoordinates = getAllPossibleCoordinates({ rows, columns });
+
 export const Input: React.FunctionComponent<UserInputProps> = ({
-  computerCordinates,
   setComputerCoordinates,
   userInput,
   setUserInput,
 }) => {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    // basic validation, do othing if input is not valid
+    const isValid = validateUserInput(allCoordinates, userInput);
+    if (!isValid) return;
 
     setComputerCoordinates((prev) =>
       prev.map((coordinateObj) =>
@@ -36,7 +45,7 @@ export const Input: React.FunctionComponent<UserInputProps> = ({
           value={userInput}
           onChange={(e) => setUserInput(e.target.value.toUpperCase())}
           placeholder='A1'
-          maxLength={3}
+          maxLength={2}
         />
       </form>
     </div>
