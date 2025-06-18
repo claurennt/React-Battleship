609531c36a1f130b1555d3351cb0166ed2b47a0a
claurennt <c.benedetti1989@gmail.com>
2025-06-18 10:57:18 +0200

    refactor: clean up Battleground component

--- src/components/battleground/Battleground.tsx
+++ src/components/battleground/Battleground.tsx
@@ -2,54 +2,61 @@ import './Battleground.css';
 import type { ComputerCoordinates } from '../../globalTypes';
 import { createGridValues } from '../utils';
 
-export const Battleground: React.FunctionComponent<{
-  computerCordinates: ComputerCoordinates;
-}> = ({ computerCordinates }) => {
-  const columns = createGridValues({ type: 'column' }); // 1–10
-  const rows = createGridValues({ type: 'row' }); // A–J
+const columns = createGridValues('column'); // A–J
+const rows = createGridValues('row'); // 1–10
 
+type RenderTableDataCellsArgs = {
+  column: string;
+  row: string;
+  computerCoordinates: ComputerCoordinates;
+};
+const renderTableDataCells = ({
+  column,
+  row,
+  computerCoordinates,
+}: RenderTableDataCellsArgs) => {
+  const cellName = `${column}${row}`;
+  const isHit = computerCoordinates.find(
+    ({ coordinate }) => coordinate === cellName
+  )?.hit;
+  const cellState = isHit ? 'hit' : 'not-hit';
+  const cellInfo = `${cellName}, ${cellState}`;
   return (
-    <table className='battleground-table'>
-      {/* hidden text for screen readers */}
-      <caption className='sr-only'>Battleground</caption>
-      <thead>
-        <tr>
-          {/* Empty corner cell */}
-          <td></td>
-          {columns.map((col) => (
-            <th key={`col-${col}`} scope='col'>
-              {col}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {rows.map((row) => {
-          return (
-            <tr key={`row-${row}`}>
-              <th scope='row'>{row}</th>
-              {columns.map((col) => {
-                const cellName = `${col}${row}`;
-                const cellState = computerCordinates.find(
-                  ({ coordinate }) => coordinate === cellName
-                )?.hit;
-                const cellInfo = `${cellName}, ${
-                  cellState ? 'hit' : 'not hit'
-                }`;
-                return (
-                  <td
-                    key={`cell-${col}-${row}`}
-                    className={cellState ? 'hit' : 'not-hit'}
-                  >
-                    {/* hidden text for screen reader users */}
-                    <p className='sr-only'>{cellInfo}</p>
-                  </td>
-                );
-              })}
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <td key={`cell-${column}-${row}`} className={cellState}>
+      {/* hidden text for screen reader users */}
+      <p className='sr-only'>{cellInfo}</p>
+    </td>
   );
 };
+
+type BattlegroundProps = {
+  computerCoordinates: ComputerCoordinates;
+};
+
+export const Battleground = ({ computerCoordinates }: BattlegroundProps) => (
+  <table className='battleground-table'>
+    {/* hidden text for screen readers */}
+    <caption className='sr-only'>Battleground</caption>
+    <thead>
+      <tr>
+        {/* Empty corner cell */}
+        <td></td>
+        {columns.map((column) => (
+          <th key={`col-${column}`} scope='col'>
+            {column}
+          </th>
+        ))}
+      </tr>
+    </thead>
+    <tbody>
+      {rows.map((row) => (
+        <tr key={`row-${row}`}>
+          <th scope='row'>{row}</th>
+          {columns.map((column) =>
+            renderTableDataCells({ column, row, computerCoordinates })
+          )}
+        </tr>
+      ))}
+    </tbody>
+  </table>
+);
