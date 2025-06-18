42efcd9a0a4c8efa866de9eb1698f783bd80b66f
claurennt <c.benedetti1989@gmail.com>
2025-06-18 10:56:41 +0200

    refactor: clean up App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
-import { Input, Battleground, ShipsAside, ships } from './components';
-import { placeShip, createGridValues } from './components/utils';
+import { Input, Battleground, ShipsInfo, ships } from './components';
+import {
+  createComputerCoordinates,
+  createGridValues,
+} from './components/utils';
 import type { ComputerCoordinates } from './globalTypes';
 
-function App() {
+const columns = createGridValues('column'); // 1–10
+const rows = createGridValues('row'); // A–J
+const allCoordinates: ComputerCoordinates = ships
+  .flatMap(({ count, size }) =>
+    createComputerCoordinates({ rows, columns, count, size }).flat()
+  )
+  .map((coordinate) => ({ coordinate, hit: false }));
+
+const App = () => {
   const [computerCoordinates, setComputerCoordinates] =
-    useState<ComputerCoordinates>([]);
-  const [userInput, setUserInput] = useState('');
-
-  const columns = createGridValues({ type: 'column' }); // 1–10
-  const rows = createGridValues({ type: 'row' }); // A–J
-
-  useEffect(() => {
-    const allCoordinates: ComputerCoordinates = ships
-      .flatMap(({ count, size }) =>
-        placeShip({ rows, columns, count, size }).flat()
-      )
-      .map((coordinate) => ({ coordinate, hit: false }));
-
-    setComputerCoordinates(allCoordinates);
-  }, []);
+    useState<ComputerCoordinates>(allCoordinates);
+  const [userInput, setUserInput] = useState<string>('');
 
   console.log(
     'cheat mode 😆',
@@ -38,11 +36,11 @@ function App() {
           setUserInput={setUserInput}
           setComputerCoordinates={setComputerCoordinates}
         />
-        <Battleground computerCordinates={computerCoordinates} />
-        <ShipsAside ships={ships} />
+        <Battleground computerCoordinates={computerCoordinates} />
+        <ShipsInfo ships={ships} />
       </main>
     </>
   );
-}
+};
 
 export default App;
